bca53d66551583098995106d240605c1a818ecd0
Jair Partida <jairpartida1@gmail.com>
2022-07-06 23:28:56 -0600

    bg changer js code and ui desktop done

---
+++ app.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body')
+const btn = document.getElementById('btn-change-color')
+const colors = ['#8963F5', '#4BEBD8', '#F56398', '#637AF5', '#FAFAFA']
+
+btn.addEventListener('click',function() {
+    const colorIndex = parseInt(Math.random()*colors.length)
+    body.style.backgroundColor = colors[colorIndex]
+})
