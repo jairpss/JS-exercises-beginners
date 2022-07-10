6e43c64094ab25c5e87adf4af96dfe4038111a1b
Jair Partida <jairpartida1@gmail.com>
2022-07-09 21:39:56 -0600

    refactoring counter JS code

--- counter.js
+++ counter.js
@@ -3,21 +3,21 @@
 let count = 0;
 
 const counter = document.getElementById('counter-num')
-// const increase = document.getElementById('plus-btn')
-// const decrease = document.getElementById('minus-btn')
+const btnIncrease = document.getElementById('plus-btn')
+const btnDecrease = document.getElementById('minus-btn')
 const restart = document.getElementById('restart-btn')
 
-function increment() {
+btnIncrease.onclick = function increment() {
     count += 1
     counter.textContent = count;
 }
 
-function decrement() {
+btnDecrease.onclick = function decrement() {
     count -= 1
     counter.textContent = count;
-}
+}   
 
-function restartNum(){
+restart.onclick = function restartNum(){
     count = 0
     counter.textContent = count;
 }
