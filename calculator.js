e9390af34c76e310fd857344e7eb712919a7c0ca
Jair Partida <jairpartida1@gmail.com>
2022-07-21 22:24:05 -0600

    calculator functionality done

--- calculator.js
+++ calculator.js
@@ -18,3 +18,41 @@ clearBtn.addEventListener("click", () => {
     answer.className = 'answer-calculator';
     answer.style.color = "#2F3E46";
 })
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        // when clicked any button num btn except delete 
+        if (!btn.id.match('delete-btn')) {
+            // To display value on btn press
+            calcScreenValue.push(btn.value)
+            currentInput.innerHTML = calcScreenValue.join('');
+
+            // To evaluate answer in real time
+            if (btn.classList.contains('btn-num')) {
+
+                answer.innerHTML = eval(calcScreenValue.join(''));
+
+            }
+        }
+
+        // When delete button is clicked
+        if (btn.id.match('delete-btn')) {
+            calcScreenValue.pop();
+            currentInput.innerHTML = calcScreenValue.join('');
+            answer.innerHTML = eval(calcScreenValue.join(''));
+        }
+
+        // When equals btn is clicked
+        if (btn.id.match('equals')) {
+            currentInput.className = 'answer-calculator';
+            answer.className = 'current-input';
+            answer.style.color = "2F3E46";
+        }
+
+        // To prevent undefined error in screen
+        if (typeof eval(calcScreenValue.join('')) == 'undefined') {
+            answer.innerHTML = 0
+        }
+
+    })
+})
