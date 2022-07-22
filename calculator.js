e8b2d5cda72868ce40496523850312a39ad72ea8
Jair Partida <jairpartida1@gmail.com>
2022-07-21 20:47:06 -0600

    functionality clear btn calculator js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,20 @@
-const input = document.getElementById('input-answer')
+const currentInput = document.getElementById('current-input')
+const answer = document.getElementById('answer-calculator')
+
 const buttons = document.querySelectorAll('button')
 const clearBtn = document.getElementById('clear-btn')
 const deleteBtn = document.getElementById('delete-btn')
 const equalBtn = document.getElementById('equals')
+
+//Calculator display
+let calcScreenValue = []
+
+//clear btn
+clearBtn.addEventListener("click", () => {
+    console.log("clear")
+    calcScreenValue = [''];
+    answer.innerHTML = 0;
+    currentInput.className = 'current-input'
+    answer.className = 'answer-calculator';
+    answer.style.color = "#2F3E46";
+})
